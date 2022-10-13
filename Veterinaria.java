7b3f055a81e700f56d8a8a878f104532f2e85e68
joss-dev <josekandia42@gmail.com>
2022-10-13 10:01:08 -0300

    iniciado

---
+++ Veterinaria.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class Veterinaria here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+
+public class Veterinaria
+{
+    private String nombre;
+    private <Animal> ArrayList animales;
+    
+    
+    //getters
+    public String getNombre() {
+        return this.nombre;
+    }
+    
+    public ArrayList <Animal> getAnimales() {
+        return this.animales;
+    }
+    public boolean ingresarAnimal(Animal p_animal) {
+            
+    }
+}
